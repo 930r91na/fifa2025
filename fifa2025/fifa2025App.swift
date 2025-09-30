4f04e8455924da653f39c72d5f4a7f15281216af
930r91na <georgina.zeronca@udlap.mx>
2025-09-30 14:18:43 -0600

    Initial Commit

---
+++ fifa2025/fifa2025App.swift
@@ -0,0 +1,17 @@
+//
+//  fifa2025App.swift
+//  fifa2025
+//
+//  Created by Georgina on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct fifa2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
